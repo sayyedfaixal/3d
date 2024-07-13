5aa1e3faaae3473214950464f5f6d3cfc26d332c
Faisal Sayed <sayedfaisal544@gmail.com>
2024-07-13 17:01:49 +0530

    Initial Commit Add Files and fix logos

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  Navbar,
+  Hero,
+  About,
+  Experience,
+  Tech,
+  Works,
+  Contact,
+  StarsCanvas,
+} from "./components";
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="relateive z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
